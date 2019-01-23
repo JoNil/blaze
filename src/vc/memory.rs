a1742fc51d39edf2b84bf5cf07c296134d73344a
Jonathan Nilsson <lord.jonil@gmail.com>
2019-01-23 00:19:18 +0000

    We free

--- src/vc/memory.rs
+++ src/vc/memory.rs
@@ -104,8 +104,6 @@ impl GpuMemory {
 impl Drop for GpuMemory {
     fn drop(&mut self) {
 
-        println!("FREE");
-
         let mut message: [u32; 7] = [
             7 * 4,
             MBOX_REQUEST,
